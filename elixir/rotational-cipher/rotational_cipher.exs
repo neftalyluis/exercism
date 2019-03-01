ab74249efd3e5d83b27757120d7c3cc2c6d03055
Neftaly Luis <neftaly.luis@resuelve.mx>
2019-02-28 21:22:39 -0600

    Ya hice Rotational Cipher

--- elixir/rotational-cipher/rotational_cipher.exs
+++ elixir/rotational-cipher/rotational_cipher.exs
@@ -1,4 +1,8 @@
 defmodule RotationalCipher do
+
+  @mayus ?A..?Z
+  @minus ?a..?z
+
   @doc """
   Given a plaintext and amount to shift by, return a rotated string.
 
@@ -8,25 +12,27 @@ defmodule RotationalCipher do
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
   def rotate(text, shift) do
-    String.to_charlist(text)
+    text
+    |> String.to_charlist()
     |> Enum.map(fn x -> add_to_codepoint(x, shift)  end)
-    |> List.to_string
+    |> List.to_string()
   end
 
-  def add_to_codepoint(character, shift) when character in ?a..?z do
-    total = character + rem(shift, 25)
-    IO.puts(total)
-    total
+  def add_to_codepoint(character, shift) when character in @minus do
+    add_to_codepoint(character, shift, ?a, ?z)
   end
 
-  def add_to_codepoint(character, shift) when character in ?A..?Z do
-    total = character + rem(shift, 25)
-    IO.puts(total)
-    total
+  def add_to_codepoint(character, shift) when character in @mayus do
+    add_to_codepoint(character, shift, ?A, ?Z)
   end
 
-  def add_to_codepoint(character, shift) do
-    IO.puts(character)
-    character
+  def add_to_codepoint(character, shift, start_of_alphabet, end_of_alphabet) do
+    case rem(shift, 26) do
+      modulo when modulo == character -> character
+      modulo when modulo + character > end_of_alphabet -> ((start_of_alphabet - 1) + (character + modulo - end_of_alphabet))
+      modulo -> character + modulo
+    end
   end
+
+  def add_to_codepoint(character, shift) when character not in @mayus or character not in @minus, do: character
 end
